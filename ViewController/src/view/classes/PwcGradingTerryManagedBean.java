00fd2ac899eb4f7b0144919c62aaa7468f79a3f1
Sarmad <sarmad@nishat.net>
2018-01-30 11:37:51 +0500

    qc points resolved and deployed 30-JAN-18

--- ViewController/src/view/classes/PwcGradingTerryManagedBean.java
+++ ViewController/src/view/classes/PwcGradingTerryManagedBean.java
@@ -59,26 +59,46 @@ public class PwcGradingTerryManagedBean {
         return (PwcOdmGradingTerryAMImpl)dc.getDataProvider();
         }
     
-    public void totalQtyValidator(FacesContext facesContext,
-                                  UIComponent uIComponent, Object object) {
-        Double totalQty = Double.parseDouble(object.toString());
+    public void validateAndSaveChanges(ActionEvent actionEvent) {
+//        Double totalQty = Double.parseDouble();
         ApplicationModuleImpl am = getApplicationModule();
         ViewObject currHeadersVO = am.findViewObject("PwcOdmNGGradingHeadersVO2");
         Double stitchingQty = Double.parseDouble(currHeadersVO.getCurrentRow().getAttribute("StitchQuantity")!=null?currHeadersVO.getCurrentRow().getAttribute("StitchQuantity").toString():"0.0");
         Double sumTotalQtyValue = Double.parseDouble(currHeadersVO.getCurrentRow().getAttribute("SumTotalQuantity")!=null?currHeadersVO.getCurrentRow().getAttribute("SumTotalQuantity").toString():"0.0");
-        if (totalQty.compareTo(stitchingQty)==1) {
-            FacesMessage message =
-                   new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                   "Invalid Value",
-                   "Total Quantity cannot exceed Stitching Quantity");
-                   facesContext.addMessage(uIComponent.getClientId(facesContext),
-                   message);
-            ((RichInputText)uIComponent).setValid(false);
+        if (sumTotalQtyValue.compareTo(stitchingQty)==1) {
+            String message =
+                   "Sum of Total Quantities cannot exceed Stitching Quantity";
+            showMessage(message, 112);
+        }
+        else {
+            ViewObject currVO = am.findViewObject("PwcOdmNGGradingLinesVO2");
+            RowSetIterator rsi = currVO.createRowSetIterator(null);
+            boolean isValid = true;
+            while(rsi.next()!=null)
+            {
+                Row currRow = rsi.getCurrentRow();
+                Double totalQty = Double.parseDouble(currRow.getAttribute("TotalQuantity")!=null?currVO.getCurrentRow().getAttribute("TotalQuantity").toString():"0.0");
+                Double gradeAQty = Double.parseDouble(currRow.getAttribute("Gradea")!=null?currRow.getAttribute("Gradea").toString():"0.0");
+                Double gradeBQty = Double.parseDouble(currRow.getAttribute("Gradeb")!=null?currRow.getAttribute("Gradeb").toString():"0.0");
+                Double gradeCQty = Double.parseDouble(currRow.getAttribute("Gradec")!=null?currRow.getAttribute("Gradec").toString():"0.0");
+                Double rewashQty = Double.parseDouble(currRow.getAttribute("Rewash")!=null?currRow.getAttribute("Rewash").toString():"0.0");
+                Double totalGradeQuantities = gradeAQty + gradeBQty + gradeCQty + rewashQty;
+                System.out.println("totalGradeQuantities = "+totalGradeQuantities);
+                    if (totalGradeQuantities.compareTo(totalQty)!=0) {
+                        String message = "Sum of Grade Quantities must be equal to Total Quantity";
+                        showMessage(message, 112);
+                        isValid = false;
+                        break;
+                    }      
+            }
+            if (isValid)
+                am.getDBTransaction().commit();
         }
         System.out.println("sumTotalQtyValue = "+sumTotalQtyValue);
     }
     
-    public void gradeAQtyValidator(FacesContext facesContext,
+    
+    /*public void gradeAQtyValidator(FacesContext facesContext,
                                    UIComponent uIComponent, Object object) {
         // Add event code here...
         Double gradeAQty = Double.parseDouble(object.toString());
@@ -168,7 +188,7 @@ public class PwcGradingTerryManagedBean {
                        message);
                 ((RichInputText)uIComponent).setValid(false);
             }
-    }
+    }*/
     
     public void deleteSelectedRows(ActionEvent actionEvent) {
         ApplicationModuleImpl am = getApplicationModule();
@@ -176,8 +196,11 @@ public class PwcGradingTerryManagedBean {
         RowSetIterator rsi = linesVO.createRowSetIterator(null);
         while (rsi.next()!=null) {
             Row currRow = rsi.getCurrentRow();
-            if ((Boolean)currRow.getAttribute("SelectedRow")==true)
+            if (currRow.getAttribute("SelectedRow")!=null)
+            {
+                if ((Boolean)currRow.getAttribute("SelectedRow")==true)
                     currRow.remove();
+            }
         }
         rsi.closeRowSetIterator();
         am.getDBTransaction().commit();
